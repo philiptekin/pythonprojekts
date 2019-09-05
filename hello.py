82ef0bdb61e4011b7422aaddbdea4c5508fbe614
philiptekin <philiptekin@users.noreply.github.com>
2019-09-05 17:24:15 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello world")
+x = eval(input('Enter a number : '))
+g = 0.0 
+felmellanrum = 0.01
+nyaG = felmellanrum**2
+gissningar = 0
+
+while (abs(g**2 - x)) >= felmellanrum:
+    g += nyaG
+    gissningar += 1
+
+print("gissningar " , gissningar)
+if abs(g**2-x) >= felmellanrum:
+    print('fail ' , x)
+else:
+    print(g , " rot som är nära " , x)
